d99189f7b822c65a02c07048a89df2c11602cc17
Xaiodo <rostislavgovbak@gmail.com>
2024-05-09 13:34:18 +0300

    Created app constant colors.

---
+++ lib/src/values/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const primary = Color(0xff0CBA70);
+  static const background = Color(0xff1E1E1E);
+
+  static const white = Color(0xffffffff);
+  static const black = Color(0xff000000);
+  static const grey = Color(0xff6E6E6E);
+}
