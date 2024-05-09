23a8163af3d4e9d8b09f91c070bdc8163b16f41c
Xaiodo <rostislavgovbak@gmail.com>
2024-05-09 13:35:01 +0300

    Added app theme and colors.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'src/values/app_colors.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,8 +11,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      darkTheme: ThemeData.dark(),
+      theme: ThemeData(
+        colorScheme: const ColorScheme.dark(
+          primary: AppColors.primary,
+          background: AppColors.background,
+        ),
+        fontFamily: 'e-Ukraine',
+      ),
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
