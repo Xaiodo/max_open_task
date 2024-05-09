88de5347ad05ac9b128e32e0ca19590347f909e1
Xaiodo <rostislavgovbak@gmail.com>
2024-05-09 15:37:17 +0300

    Added new app strings and new colors.

--- lib/src/values/app_colors.dart
+++ lib/src/values/app_colors.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static const primary = Color(0xff0CBA70);
   static const background = Color(0xff1E1E1E);
+  static const onSecondary = Color(0xff6E6E6E);
+  static const textInputBackground = Color(0xff292929);
 
   static const white = Color(0xffffffff);
   static const black = Color(0xff000000);
-  static const grey = Color(0xff6E6E6E);
+  static const transparent = Color(0x00000000);
 }
---
+++ lib/src/values/app_strings.dart
@@ -0,0 +1,7 @@
+abstract class AppStrings {
+  static const howToAddress = 'Як до Вас звертатися?';
+  static const pleaseEnterName =
+      'Будь ласка, укажіть ваше ім\'я для персоналізації сервісу.';
+  static const nameHint = 'Ваше ім\'я';
+  static const continueText = 'Продовжити';
+}
