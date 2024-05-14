515b8bbb00c45b1afde1f84b801729fc550095af
Xaiodo <rostislavgovbak@gmail.com>
2024-05-14 15:37:50 +0300

    Updated HomeScreenConstants with new card and container border radius values.

--- lib/src/values/app_constants.dart
+++ lib/src/values/app_constants.dart
@@ -14,7 +14,9 @@ abstract class NameScreenConstants {
 }
 
 abstract class HomeScreenConstants {
-  static const double borderRadius = 40.0;
+  static const double cardBorderRadius = 40.0;
+  static const double containerBorderRadius = 55.0;
+  static const double buttonBorderRadius = 19.0;
   static const double topPadding = 10.0;
   static const double sidePadding = 22.0;
   static const double bottomPadding = 22.0;
@@ -22,14 +24,37 @@ abstract class HomeScreenConstants {
   static const double spacing = 20.0;
   static const double innerSpacing = 20.0;
   static const double fractionWidth = 0.7;
-  static const double iconSize = 10.0;
+  static const double iconSize = 8.0;
+  static const double iconHeight = 30.0;
+  static const double iconLeftSpacing = 6.2;
   static const double marginVertical = 22.0;
   static const double marginHorizontal = 25.0;
-  static const double widthPercentage = 0.88;
-  static const double heightPercentage = 0.4;
+  static const double imageHeightPercentage = 0.4;
+  static const double imageWidthPercentage = 0.88;
   static const double navSidePadding = 40.0;
   static const double bottomNavPadding = 7.0;
+  static const double containerIconPadding = 17.0;
+  static const double containerIconBorderRadius = 17.0;
 
-  static const String imageAssetPath = 'assets/images/image.png';
+  static const String carImagePath = 'assets/images/image.png';
+  static const String wifiIconPath = 'assets/icons/wifi.svg';
+  static const String geolocationImagePath = 'assets/images/location.png';
   static const double imageScale = 0.5;
+
+  static const String homeScreenCardTitle = 'Привіт, \n';
+  static const String homeScreenCardSubtitle = 'Додавання авто';
+  static const String homeScreenCardText =
+      'Завантажте дані про ваше авто для кращого використання сервісу.';
+  static const String homeScreenCardTextAddCar = 'Додати авто';
+
+  static const String homeContainerTextInternetError = 'Відсутній зв\'язок';
+  static const String homeContainerTextInternetErrorSubtitle =
+      'Відсутність доступу до геолокації.\nПереконайтеся, що у додатку увімкнено '
+      'геолокацію та перевірте з\'єднання з Інтернетом.';
+  static const String homeContainerTextConfigureGeolocation =
+      'Налаштувати геолокацію';
+
+  static const String homeScreenButtonText = 'Викликати майстра';
+
+  static const String homeContainerTextLoading = 'Завантаження...';
 }
